69a869e81132b229f55fcc950b05e7179fb81e6d
stn3asME <223004085@stu.ukzn.ac.za>
2026-04-11 20:28:52 +0200

    CLick commit, ussually the message here is useful.

--- DrivingSchoolSystem/Form1.cs
+++ DrivingSchoolSystem/Form1.cs
@@ -16,5 +16,12 @@ namespace DrivingSchoolSystem
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Fanele kube ukuthi uloged in with your git account
+            //fanele wenze amachanges kwi branch ehlukile no main branch as follows...
+            //then code until ube satified nge code yakho, after that uzoya ku gitChanges
+        }
     }
 }

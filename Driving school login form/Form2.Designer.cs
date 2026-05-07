07dc56760c99ca4349a854c83b842a9641e8b5be
stn3asME <223004085@stu.ukzn.ac.za>
2026-05-07 02:25:19 +0200

    Changed server password from **** to correct one. Chnaged query to include email instead of username which is not present in staff table. changed [password] to password.

---
+++ Driving school login form/Form2.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Driving_school_login_form
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.sTUDENTSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registerStudentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bookLessonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iNSTRUCTORSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateStudentProgressToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sCHEDULEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewScheduleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sTUDENTSToolStripMenuItem,
+            this.iNSTRUCTORSToolStripMenuItem,
+            this.sCHEDULEToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // sTUDENTSToolStripMenuItem
+            // 
+            this.sTUDENTSToolStripMenuItem.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("sTUDENTSToolStripMenuItem.BackgroundImage")));
+            this.sTUDENTSToolStripMenuItem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.sTUDENTSToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registerStudentToolStripMenuItem,
+            this.bookLessonToolStripMenuItem});
+            this.sTUDENTSToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sTUDENTSToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("sTUDENTSToolStripMenuItem.Image")));
+            this.sTUDENTSToolStripMenuItem.Name = "sTUDENTSToolStripMenuItem";
+            this.sTUDENTSToolStripMenuItem.Size = new System.Drawing.Size(90, 21);
+            this.sTUDENTSToolStripMenuItem.Text = "Students";
+            // 
+            // registerStudentToolStripMenuItem
+            // 
+            this.registerStudentToolStripMenuItem.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.registerStudentToolStripMenuItem.Name = "registerStudentToolStripMenuItem";
+            this.registerStudentToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.registerStudentToolStripMenuItem.Text = "Register Student";
+            // 
+            // bookLessonToolStripMenuItem
+            // 
+            this.bookLessonToolStripMenuItem.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.bookLessonToolStripMenuItem.Name = "bookLessonToolStripMenuItem";
+            this.bookLessonToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.bookLessonToolStripMenuItem.Text = "Book Lesson";
+            // 
+            // iNSTRUCTORSToolStripMenuItem
+            // 
+            this.iNSTRUCTORSToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.updateStudentProgressToolStripMenuItem});
+            this.iNSTRUCTORSToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iNSTRUCTORSToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("iNSTRUCTORSToolStripMenuItem.Image")));
+            this.iNSTRUCTORSToolStripMenuItem.Name = "iNSTRUCTORSToolStripMenuItem";
+            this.iNSTRUCTORSToolStripMenuItem.Size = new System.Drawing.Size(102, 21);
+            this.iNSTRUCTORSToolStripMenuItem.Text = "Instructors";
+            // 
+            // updateStudentProgressToolStripMenuItem
+            // 
+            this.updateStudentProgressToolStripMenuItem.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.updateStudentProgressToolStripMenuItem.Name = "updateStudentProgressToolStripMenuItem";
+            this.updateStudentProgressToolStripMenuItem.Size = new System.Drawing.Size(229, 22);
+            this.updateStudentProgressToolStripMenuItem.Text = "Update Student Progress";
+            // 
+            // sCHEDULEToolStripMenuItem
+            // 
+            this.sCHEDULEToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewScheduleToolStripMenuItem});
+            this.sCHEDULEToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sCHEDULEToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("sCHEDULEToolStripMenuItem.Image")));
+            this.sCHEDULEToolStripMenuItem.Name = "sCHEDULEToolStripMenuItem";
+            this.sCHEDULEToolStripMenuItem.Size = new System.Drawing.Size(97, 21);
+            this.sCHEDULEToolStripMenuItem.Text = "Schedules";
+            this.sCHEDULEToolStripMenuItem.Click += new System.EventHandler(this.sCHEDULEToolStripMenuItem_Click);
+            // 
+            // viewScheduleToolStripMenuItem
+            // 
+            this.viewScheduleToolStripMenuItem.Name = "viewScheduleToolStripMenuItem";
+            this.viewScheduleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.viewScheduleToolStripMenuItem.Text = "View Schedule";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.SystemColors.HotTrack;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.linkLabel1.LinkColor = System.Drawing.Color.White;
+            this.linkLabel1.Location = new System.Drawing.Point(733, 9);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "LOG OUT";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form2";
+            this.Text = "Dashboard";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sTUDENTSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iNSTRUCTORSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sCHEDULEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registerStudentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bookLessonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateStudentProgressToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewScheduleToolStripMenuItem;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}

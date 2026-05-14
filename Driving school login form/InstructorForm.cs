d6b42bacdd9a0180261d4dfba7cb37bea8f4f16d
Asibonge Marhwa (223055111) <223055111@stu.ukzn.ac.za>
2026-05-14 16:13:49 +0200

    Added Manager,Receptionist and Instructor Forms

---
+++ Driving school login form/InstructorForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Driving_School_System_Forms
+{
+    public partial class InstructorForm : Form
+    {
+        public InstructorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Driving school login form/Program.cs
+++ Driving school login form/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Driving_school_login_form
+namespace Driving_School_System_Forms
 {
     internal static class Program
     {
@@ -16,7 +16,7 @@ namespace Driving_school_login_form
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ManagerForm());
         }
     }
 }
